831a76340ac552193b24a6b343ab7b195bfbed68
Ashutosh-Rane <ashutoshranead@gmail.com>
2026-07-28 16:33:49 +0530

    455

--- jobs/views.py
+++ jobs/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .jobposting import process_job
 
 
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def add_job(request):
 
     result = process_job(request.data)
