e58847937c9becbd05e647f37ad2a06a27d9291c
Ashutosh-Rane <ashutoshranead@gmail.com>
2026-07-28 16:55:39 +0530

    sheet

--- jobs/jobposting.py
+++ jobs/jobposting.py
@@ -1,11 +1,18 @@
+
+from google.oauth2.service_account import Credentials
+
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxwTm1COWzzNO4zJ-cgPbrHwzyyguhUTClRJI9V4Gzhw3oISB5suMLPsor87SilE2Xi/exec"  # paste your URL here
+
 def process_job(data):
 
+    response = data.post(WEB_APP_URL, json=data)
+    response.raise_for_status()
     print("========= JOB RECEIVED =========")
 
     print("Job Name :", data.get("job_name"))
     print("Job ID   :", data.get("job_id"))
     print("Location :", data.get("location"))
-    print("Exp      :", data.get("experience"))
+    print("Exp      :", data.get("exp"))
     print("Education:", data.get("education"))
     print("Skills   :", data.get("skills"))
     print("Description:", data.get("description"))
