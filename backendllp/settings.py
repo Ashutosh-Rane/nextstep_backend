8901310acaedef67e6c7950dcc6dc2c352d5ec60
Ashutosh-Rane <ashutoshranead@gmail.com>
2026-07-28 16:22:01 +0530

    cors

--- backendllp/settings.py
+++ backendllp/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 
-CORS_ALLOWED_ORIGINS = [True]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
